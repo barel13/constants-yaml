301f912e65eaaa139c0c30908912c981279dd1c0
Barel Shlidor <Barel@shlidor.org>
2022-12-03 18:37:10 +0200

    Add support for networktable

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -11,35 +11,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.example.Main.debug;
+
 public class Constants {
     private static final Map<String, WebConstant> constants = new HashMap<>();
-
-    public static void load() throws IOException {
-        var resource = Thread.currentThread().getContextClassLoader().getResource("constants.yaml");
-
-        var mapper = new ObjectMapper(new YAMLFactory());
-        mapper.registerModule(new Jdk8Module());
-        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-        if (resource == null) {
-            System.out.println("Couldn't find resource");
-            return;
-        }
-
-        var typeRef = new TypeReference<Map<String, List<Constant>>>() {
-        };
-        var values = mapper.readValue(resource.openStream(), typeRef);
-        values.forEach((table, value) -> {
-            for (var constant : value) {
-                String path = String.join(".", table, constant.getKey());
-                if (constant.isTunable()) {
-                    constants.put(path, new NetworkTableConstant(constant.getValue().doubleValue()));
-                } else {
-                    constants.put(path, new WrapperConstant(constant.getValue().doubleValue()));
-                }
-            }
-        });
-    }
+    private static boolean initialized = false;
 
     static {
         try {
@@ -50,6 +26,61 @@ public class Constants {
         }
     }
 
+    public static void load() throws IOException {
+        Map<String, List<Constant>> values = getValues();
+        if (values == null) {
+            System.out.println("Couldn't find resource");
+            return;
+        }
+
+        values.forEach((table, value) -> {
+            for (var constant : value) {
+                String path = String.join(".", table, constant.getKey());
+                WebConstant constantToSave = getConstant(constant, path);
+                constants.put(path, constantToSave);
+            }
+        });
+    }
+
+    private static WebConstant getConstant(Constant constant, String path) {
+        if (constant.isTunable() && debug) {
+            var constantToSave = new NetworkTableConstant(constant.getValue().doubleValue());
+            if (initialized) {
+                initialize(path, constantToSave);
+            }
+            return constantToSave;
+        }
+        return new WrapperConstant(constant.getValue().doubleValue());
+    }
+
+    private static Map<String, List<Constant>> getValues() throws IOException {
+        var resource = Thread.currentThread().getContextClassLoader().getResource("constants.yaml");
+
+        var mapper = new ObjectMapper(new YAMLFactory());
+        mapper.registerModule(new Jdk8Module());
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+        if (resource == null) {
+            return null;
+        }
+
+        var typeRef = new TypeReference<Map<String, List<Constant>>>() {
+        };
+        return mapper.readValue(resource.openStream(), typeRef);
+    }
+
+    private static void initialize(String path, WebConstant constant) {
+        if (constant instanceof NetworkTableConstant) {
+            String[] split = path.split("\\.");
+            ((NetworkTableConstant) constant).initialize(split[0], split[1]);
+        }
+    }
+
+    public static void initializeAll() {
+        constants.forEach(Constants::initialize);
+        initialized = true;
+    }
+
     public static WebConstant get(String path) {
         if (constants.containsKey(path)) {
             return constants.get(path);
@@ -57,4 +88,7 @@ public class Constants {
         return new WrapperConstant(0);
     }
 
+    public static Map<String, WebConstant> getConstants() {
+        return constants;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
 public class Main {
+    public static boolean debug = true;
 
     public static void main(String[] args) {
 
         System.out.println(Constants.get("SwerveDrive.kP"));
+        System.out.println(Constants.getConstants());
+
+//        Constants.initializeAll();
     }
 }
